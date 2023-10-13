8f746d441f861bc0e7f9ed94c73a13750ee1a405
aminekell <132770697+aminekell@users.noreply.github.com>
2023-10-13 19:18:19 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+int main(int ac, char **av)
+{
+    char *line = NULL, **command = NULL;
+    int status = 0;
+    (void) ac;
+
+    while (1)
+    {
+        line = read_line();
+        if (line == NULL) /* reached EOF, Ctrl + D */
+        {
+            if (isatty(STDIN_FILENO))
+            {
+                write(STDOUT_FILENO, "\n", 1);
+            }
+            return (status);
+        }
+
+        command = tokenizer(line);
+        if (!command)
+            continue;
+        status = _execute(command, av);
+    }
+}
---
+++ shell.h
@@ -0,0 +1,30 @@
+#ifndef shell_h
+#define shell_h
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#define DELIM " \t\n"
+ 
+extern char **environ;
+
+char *read_line (void);
+char **tokenizer(char *line);
+int _execute(char **command, char **argv);
+
+char *_strdup (const char *str);
+int _strcmp(char *s1, char *s2);
+int _strlen(char  *s);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+
+void freearray2D(char **arr);
+
+
+#endif
