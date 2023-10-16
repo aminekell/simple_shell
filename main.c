b3399d61baf5b5f5edc959ebd7af2a284ceb5d61
you-desing <youssefboulahya3@gmail.com>
2023-10-16 04:35:17 -0700

    betty style

--- main.c
+++ main.c
@@ -1,34 +1,36 @@
 #include "shell.h"
+/**
+* main - Simple Shell main function
+* @ac: Count of argumnents
+* @av: Arguments
+* Return: Always (success).
+*/ 
+int main(int ac, char **av)
+{
+	char *line = NULL, **alternative_command = NULL;
+	int alternative_status = 0;
+	(void) ac;
 
-int main(int ac, char **av) {
-    char *line = NULL, **alternative_command = NULL;
-    int alternative_status = 0;
-    (void) ac; /* Silence the unused argument warning */
+	while (1)
+	{
+	if (isatty(STDIN_FILENO))
+	{
+		printf("$ ");
+		fflush(stdout);
+	}
+	line = custom_read_line();
+	if (line == NULL)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			printf("\n");
+		}
+		return (alternative_status);
+	}
+	alternative_command = my_tokenizer(line);
 
-    while (1) {
-        if (isatty(STDIN_FILENO)) {
-            printf("$ ");
-            fflush(stdout);
-        }
-
-        line = custom_read_line();
-
-        if (line == NULL) {
-            /* Reached EOF, Ctrl + D */
-            if (isatty(STDIN_FILENO)) {
-                printf("\n");
-            }
-            return (alternative_status);
-        }
-
-        /* Tokenize the input line */
-        alternative_command = my_tokenizer(line);
-
-        if (!alternative_command) {
-            continue;
-        }
-
-        /* Execute the alternative command */
-        alternative_status = execute_alternative(alternative_command, av);
-    }
+	if (!alternative_command)
+		continue;
+	alternative_status = execute_alternative(alternative_command, av);
+	}
 }
--- read.c
+++ read.c
@@ -1,19 +1,23 @@
 #include "shell.h"
 #include <stdio.h>
+/**
+* custom_read_line - function read the prompt
+*
+* Return: return custom_line or NULL.
+*/ 
+char *custom_read_line(void)
+{
+	char *custom_line = NULL;
+	size_t custom_buffer_size = 0;
+	ssize_t custom_length;
 
-/* الدالة لقراءة السطر المخصصة */
-char *custom_read_line(void) {
-    char *custom_line = NULL;     /* السلسلة المستلمة */
-    size_t custom_buffer_size = 0;  /* حجم الذاكرة المخصصة */
-    ssize_t custom_length;          /* طول السطر المقروء */
+	custom_length = getline(&custom_line, &custom_buffer_size, stdin);
 
-    custom_length = getline(&custom_line, &custom_buffer_size, stdin);
+	if (custom_length == -1)
+	{
+	free(custom_line);
+	return (NULL);
+	}
 
-    if (custom_length == -1) {
-        /* إذا فشلت القراءة، قم بتحرير الذاكرة وارجاع قيمة فارغة */
-        free(custom_line);
-        return NULL;
-    }
-
-    return custom_line;
+	return (custom_line);
 }
