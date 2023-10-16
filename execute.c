2623352d4457f0b4dde697b7dec954cb51c9b14d
you-desing <youssefboulahya3@gmail.com>
2023-10-16 14:13:52 -0700

    code

--- execute.c
+++ execute.c
@@ -8,32 +8,36 @@
 int execute_alternative(char **commands, char **arguments)
 {
 	int index;
-	pid_t child_process;
-	int status;
+    pid_t child_process;
+    int status;
 
-	child_process = fork();
-	if (child_process == 0)
-	{
-	if (execve(commands[0], commands, environ) == -1)
-	{
-		perror(arguments[0]);
+    child_process = fork();
 
-		for (index = 0; commands[index]; index++)
-		{
-			free(commands[index]), commands[index] = NULL;
-		}
-		free(commands), commands = NULL;
-		exit(0);
-	}
-	}
-	else
-	{
-		waitpid(child_process, &status, 0);
-		for (index = 0; commands[index]; index++)
-		{
-			free(commands[index]), commands[index] = NULL;
-		}
-		free(commands), commands = NULL;
-	}
+    if (child_process == 0) {
+        /* Child process */
+        if (execve(commands[0], commands, environ) == -1) {
+            perror(arguments[0]);
+
+            /* Free memory and exit */
+            for (index = 0; commands[index]; index++) {
+                free(commands[index]);
+                commands[index] = NULL;
+            }
+            free(commands);
+            commands = NULL;
+            exit(0);
+        }
+    } else {
+        /* Parent process */
+        waitpid(child_process, &status, 0);
+
+        /* Free memory */
+        for (index = 0; commands[index]; index++) {
+            free(commands[index]);
+            commands[index] = NULL;
+        }
+        free(commands);
+        commands = NULL;
+    }
 	return (WEXITSTATUS(status));
 }
