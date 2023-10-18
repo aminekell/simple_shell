7422104b3c2678222a184b3d398576b4e9c3d3c9
aminekell <aminekella2003.com>
2023-10-18 07:45:12 -0700

    done

--- read.c
+++ read.c
@@ -3,17 +3,15 @@
 
 /* الدالة لقراءة السطر المخصصة */
 char *custom_read_line(void) {
-    char *custom_line = NULL;     /* السلسلة المستلمة */
-    size_t custom_buffer_size = 0;  /* حجم الذاكرة المخصصة */
-    ssize_t custom_length;          /* طول السطر المقروء */
-
-    custom_length = getline(&custom_line, &custom_buffer_size, stdin);
-
-    if (custom_length == -1) {
+char *custom_line = NULL;     /* السلسلة المستلمة */
+size_t custom_buffer_size = 0;  /* حجم الذاكرة المخصصة */
+ssize_t custom_length;          /* طول السطر المقروء */
+custom_length = getline(&custom_line, &custom_buffer_size, stdin);
+if (custom_length == -1) {
         /* إذا فشلت القراءة، قم بتحرير الذاكرة وارجاع قيمة فارغة */
-        free(custom_line);
-        return NULL;
+free(custom_line);
+return NULL;
     }
 
-    return custom_line;
+return custom_line;
 } 
